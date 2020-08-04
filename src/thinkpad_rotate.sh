b95b75432d307b2d1277dba3355a176f66211473
Robin SHAMSNEJAD <rsham@free.fr>
2020-08-04 11:22:41 +0200

    Fixed missing quotes

--- src/thinkpad_rotate.sh
+++ src/thinkpad_rotate.sh
@@ -12,29 +12,29 @@ case "$rotation" in
     normal)
     # rotate to the left
     xrandr -o left
-    xsetwacom set Wacom ISDv4 E6 Pen stylus rotate ccw
-    xsetwacom set Wacom ISDv4 E6 Pen eraser rotate ccw
-    xsetwacom set Wacom ISDv4 E6 Finger touch rotate ccw
+    xsetwacom set "Wacom ISDv4 E6 Pen stylus" rotate ccw
+    xsetwacom set "Wacom ISDv4 E6 Pen eraser" rotate ccw
+    xsetwacom set "Wacom ISDv4 E6 Finger touch" rotate ccw
     ;;
     left)
     # rotate to inverted
     xrandr -o inverted
-    xsetwacom set Wacom ISDv4 E6 Pen stylus rotate half
-    xsetwacom set Wacom ISDv4 E6 Pen eraser rotate half
-    xsetwacom set Wacom ISDv4 E6 Finger touch rotate half
+    xsetwacom set "Wacom ISDv4 E6 Pen stylus" rotate half
+    xsetwacom set "Wacom ISDv4 E6 Pen eraser" rotate half
+    xsetwacom set "Wacom ISDv4 E6 Finger touch" rotate half
     ;;
     inverted)
     # rotate to the right
     xrandr -o right
-    xsetwacom set Wacom ISDv4 E6 Pen stylus rotate cw
-    xsetwacom set Wacom ISDv4 E6 Pen eraser rotate cw
-    xsetwacom set Wacom ISDv4 E6 Finger touch rotate cw
+    xsetwacom set "Wacom ISDv4 E6 Pen stylus" rotate cw
+    xsetwacom set "Wacom ISDv4 E6 Pen eraser" rotate cw
+    xsetwacom set "Wacom ISDv4 E6 Finger touch" rotate cw
     ;;
     right)
     # rotate to normal
     xrandr -o normal
-    xsetwacom set Wacom ISDv4 E6 Pen stylus rotate none
-    xsetwacom set Wacom ISDv4 E6 Pen eraser rotate none
-    xsetwacom set Wacom ISDv4 E6 Finger touch rotate none
+    xsetwacom set "Wacom ISDv4 E6 Pen stylus" rotate none
+    xsetwacom set "Wacom ISDv4 E6 Pen eraser" rotate none
+    xsetwacom set "Wacom ISDv4 E6 Finger touch" rotate none
     ;;
 esac
